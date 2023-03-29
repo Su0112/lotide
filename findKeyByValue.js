840d53a1f54e0c64b8d5b4a1f7baf1692ca5f23e
SuShruthi <shruthacm@gmail.com>
2023-03-29 07:18:14 -0700

    executed the challenge

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,14 +9,29 @@ const assertEqual = function(actual, expected) {
 
 };
 
+function findKeyByValue(object, value){
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+
+}
 
 
+// const numbers = { 
+//   one: 1,
+//   two: 2,
+//   three: 3
+// };
 
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
-
+//console.log(findKeyByValue(numbers, 3))
+//console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
