c860a583609fcce4939ebd38b21bcb080dbe7af3
SuShruthi <shruthacm@gmail.com>
2023-03-29 07:06:46 -0700

    add besrTVShowsByGenre function to the file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,3 +8,15 @@ const assertEqual = function(actual, expected) {
     }
 
 };
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
