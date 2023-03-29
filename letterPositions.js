bd6275ac7f275acaf8bc86cee2555f7a84dbd608
SuShruthi <shruthacm@gmail.com>
2023-03-29 06:57:43 -0700

    add letterPositions.js file

--- letterPositions.js
+++ letterPositions.js
@@ -26,19 +26,19 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 function letterPositions(sentence){
-  const position = {};
+  const results = {};
   //logic
   for (let i = 0; i < sentence.length; i++){
         //condional statement to count letters
       if (sentence[i] !== ' ') { //if the current character is not a space or empty:
-      if (position[sentence[i]]) { // if the counts object already has an entry for this character:
-        position[sentence[i]].push(i);
+      if (results[sentence[i]]) { // if the counts object already has an entry for this character:
+        results[sentence[i]].push(i);
       } else {
-        position[sentence[i]] = [i]; //else: set the count of the current character to 1
+        results[sentence[i]] = [i]; //else: set the count of the current character to 1
       }
     }
   }
-  return position;
+  return results;
 }
 
 assertArraysEqual(letterPositions("hello").e, [1]);
