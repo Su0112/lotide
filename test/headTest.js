c04683a53c2c517c440ebc37f7686f645a530f7c
SuShruthi <shruthacm@gmail.com>
2023-04-14 16:20:59 -0700

    added mocha in the headTest

--- test/headTest.js
+++ test/headTest.js
@@ -9,9 +9,3 @@ describe("#head", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
 });
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-
-// // TEST CODE
-// assertEqual(head([5, 6, 7]), 5)
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
