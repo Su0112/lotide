119bbc960868d48beaefbaf32e9cedc46290f04d
SuShruthi <shruthacm@gmail.com>
2023-04-14 09:05:32 -0700

    included test for tail

--- tail.js
+++ tail.js
@@ -1,14 +1,9 @@
-const assertEqual = function(actual, expected) {
-    if (actual != expected){
-        console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`)
-    } else {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    }
-
-};
+const assertEqual = require('./assertEqual');
 // FUNCTION IMPLEMENTATION
-const tail = function(arr){
+const tail = function(arr) {
     let newArray = arr.slice(1);
     return newArray;
 }
-assertEqual(tail(["Hello", "Lighthouse"]), "Lighthouse")
+
+
+module.exports = tail;
