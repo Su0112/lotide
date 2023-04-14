3e5ea1c860aad29197b77dafd8a2bc471375ef98
SuShruthi <shruthacm@gmail.com>
2023-04-14 08:34:18 -0700

    included test folder

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,4 @@ const assertEqual = function(actual, expected) {
 
 };
 
-/*// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp"); //Assertion Failed: Lighthouse Labs !== Bootcamp
-assertEqual(1, 1); //Assertion Passed: 1 === 1 */
-
 module.exports = assertEqual;
