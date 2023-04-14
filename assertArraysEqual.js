a13914fc97fa53c7659990da78bc8c3f9362c5e4
SuShruthi <shruthacm@gmail.com>
2023-04-14 09:38:03 -0700

    add assertArrayEqualTest file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,5 @@
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
-// // FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//     if (actual != expected){
-//         console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`)
-//     } else {
-//         console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-//     }
-
-// };
-// function eqArrays(arr1, arr2) {
-//   // If the arrays have different lengths, they can't be equal
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   // Compare each element of the arrays
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-
-//   // If all elements are equal, the arrays are equal
-//   return true;
-// }
-
-
-
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
