deec16fa8a5aac8d56018c05edc57f1786e2d5e4
SuShruthi <shruthacm@gmail.com>
2023-04-14 09:32:30 -0700

    add assertArrayEqualTest file

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should log "✅✅✅ Assertion Passed: [1,2,3] === [1,2,3]"
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // should log "🛑🛑🛑 Assertion Failed: [1,2,3] !== [3,2,1]"
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // should log "🛑🛑🛑 Assertion Failed: [1,2,3] !== [3,2,1]"
