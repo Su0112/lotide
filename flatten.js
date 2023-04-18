f14bdbbf5c30d2cd977a7f8f563a6048ffc0480a
SuShruthi <shruthacm@gmail.com>
2023-04-18 07:50:59 -0700

    removed unnecessary test functions from flatten.js

--- flatten.js
+++ flatten.js
@@ -1,26 +1,4 @@
 // FUNCTION IMPLEMENTATION
-function eqArrays(arr1, arr2) {
-  // If the arrays have different lengths, they can't be equal
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // Compare each element of the arrays
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // If all elements are equal, the arrays are equal
-  return true;
-}
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const flatten = function(arr) {
   let flatArr = [];
   for (let element of arr) {
@@ -33,12 +11,7 @@ const flatten = function(arr) {
     }
   }
   return flatArr;
-}
-
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-// should pass
-assertArraysEqual(flatten([1, [2, [3, [4]]]]), [1, 2, [3, [4]]]);
-// should fail
+};
 
 module.exports = flatten;
 
