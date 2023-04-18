99b6ce8b31bce92af0dce90ae7e9b87dff0f12f6
SuShruthi <shruthacm@gmail.com>
2023-04-18 07:55:20 -0700

    refactored eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,39 +1,13 @@
 // FUNCTION IMPLEMENTATION
-function eqArrays(arr1, arr2) {
-  // If the arrays have different lengths, they can't be equal
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // Compare each element of the arrays
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // If all elements are equal, the arrays are equal
-  return true;
-}
-const assertEqual = function(actual, expected) {
-  if (actual != expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-const longSleeveShirtObject = {
-  size: "medium",
-  color: "red",
-  sleeveLength: "long",
-};
+const eqArrays = require("./eqArrays");
+
 const eqObjects = function(object1, object2) {
-  let keys1 = Object.keys(object1);
-  let keys2 = Object.keys(object2);
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) {
     return false;
   }
-  for (let key of keys1) {
+  for (const key of keys1) {
     if (Array.isArray(object1[key] && Array.isArray(object2[key]))) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
@@ -44,11 +18,5 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-eqObjects(shirtObject, anotherShirtObject);
-// => true
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-eqObjects(shirtObject, longSleeveShirtObject);
-// => false
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 
 module.exports = eqObjects;
