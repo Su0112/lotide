8ab40226786dcc7ce9b64c89c676c69962430d0d
SuShruthi <shruthacm@gmail.com>
2023-04-18 02:21:31 -0700

    implementing feedback as mentioned

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,19 @@
 // FUNCTION IMPLEMENTATION
 function countLetters(sentence) {
-  const counts = {}; //initial count
+  //initial count
+  const counts = {};
   for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
     //condional statement to count letters
+    //if the current character is not a space or empty
     if (letter !== ' ') {
-      //if the current character is not a space or empty:
+      // if the counts object already has an entry for this character:
       if (counts[letter]) {
-        // if the counts object already has an entry for this character:
-        counts[letter]++;
         //increment the count of the current character by 1
+        counts[letter]++;
       } else {
-        counts[letter] = 1;
         //else: set the count of the current character to 1
+        counts[letter] = 1;
       }
     }
   }
--- letterPositions.js
+++ letterPositions.js
@@ -27,11 +27,14 @@ function letterPositions(sentence) {
   //logic
   for (let i = 0; i < sentence.length; i++) {
     //condional statement to count letters
-    if (sentence[i] !== ' ') { //if the current character is not a space or empty:
-      if (results[sentence[i]]) { // if the counts object already has an entry for this character:
+    if (sentence[i] !== ' ') {
+      //if the current character is not a space or empty:
+      if (results[sentence[i]]) {
+        // if the counts object already has an entry for this character:
         results[sentence[i]].push(i);
       } else {
-        results[sentence[i]] = [i]; //else: set the count of the current character to 1
+        results[sentence[i]] = [i];
+        //else: set the count of the current character to 1
       }
     }
   }
