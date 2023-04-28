8fde0de6dc1ebbf5ebdd214d38e04cf7e6ccf491
SuShruthi <shruthacm@gmail.com>
2023-04-28 00:28:38 -0700

    changed assetArrayEqual

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,14 @@
+const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 
 
 // TEST CODE
-assertArraysEqual(eqArrays(["1", 2, "3"], [2, 3, 4]), false); // => pass
-assertArraysEqual(eqArrays([4, 5, 6], [4, 5, 6]), true); // => pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// should log "✅✅✅ Assertion Passed: [1,2,3] === [1,2,3]"
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// should log "🛑🛑🛑 Assertion Failed: [1,2,3] !== [3,2,1]"
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+// should log "🛑🛑🛑 Assertion Failed: [1,2,3] !== [3,2,1]"
