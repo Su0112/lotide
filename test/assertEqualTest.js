f7d2c778715e770e76272cd8c63fe26f3bf60736
SuShruthi <shruthacm@gmail.com>
2023-04-28 10:52:50 -0700

    add test code to assertEqualTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,2 +1,4 @@
 const assertEqual = require('../assertEqual');
 // TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); //Assertion Failed: Lighthouse Labs !== Bootcamp
+assertEqual(1, 1); //Assertion Passed: 1 === 1
